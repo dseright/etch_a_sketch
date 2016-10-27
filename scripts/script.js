155c646b7bbae174a1f46373bcd7792dcf954c0f
Dustin Seright <dseright@gmail.com>
2016-10-27 11:01:32 -0500

    initial file set up, first idea for grid

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	function grid(input) {
+		// saved indent to check if input will crash/lag browser
+			for(i = 0, i < input, i++){
+				for(j = 0, j < input, j++){
+					//code to add a single div which will iterate to input
+				}
+			}
+	}
+})
