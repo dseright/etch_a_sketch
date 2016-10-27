256aa81ecb45bc15e7685b49c2e86dbd819e2f99
Dustin Seright <dseright@gmail.com>
2016-10-27 11:09:38 -0500

    Few more trivial things

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
 	function grid(input) {
-		// saved indent to check if input will crash/lag browser
+		if(input =< 128){
 			for(i = 0, i < input, i++){
 				for(j = 0, j < input, j++){
 					//code to add a single div which will iterate to input
+					//double check this is how to append inside the container div
+					$(#sketch).append("<div class="etch"></div>");
 				}
 			}
+		} else {
+			// code to display that input value was too high.
+		}
 	}
 })
