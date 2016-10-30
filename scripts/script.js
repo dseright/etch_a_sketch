a72758f7093b6c9c313198677660a84d4b2ad114
Dustin Seright <dseright@gmail.com>
2016-10-30 09:49:21 -0500

    Basic code done, only fixed width left to solve.

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,32 @@
 function grid(size) {
 	$('#sketch').html('<br>');
-	if(size <= 128){
+	if(size <= 65){
 		console.log(size);
 		for(var i = 0; i < size; i++) {
-			$('#sketch').append('<div class="row"></div>')
+			$('#sketch').append('<div class="col"></div>')
 		}
 		for(var j = 0; j < size; j++){
 				//code to add a single div which will iterate to input
-				$('.row').append('<div class="cell"></div>');
+				$('.col').append('<div class="cell"></div>');
 		}
 	} else {
 		// code to display that input value was too high.
 		console.log(size);
-		} 
+		}; 
+	$('.cell').mouseenter(function(){
+		$(this).addClass('blk');
+	});
 };
 
 
 $(document).ready(function() {
+	grid(16);
 	$('#gridrender').click(function() {
 		var gdsize = $('#gridsize').val();
 		grid(gdsize);
 	});
+	$('#clear').click(function(){
+		$('#sketch').find('div').removeClass('blk');
+	});
+ 
 });
