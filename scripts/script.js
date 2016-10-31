c2ace57ef2eb0d76ee49c82e22d2cd26ce37f93a
Dustin Seright <dseright@gmail.com>
2016-10-31 06:17:37 -0500

    Random Colors add and working, grid added, default added.

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,14 @@
 function grid(size) {
 	$('#sketch').html('<br>');
+	var style = 'default';
+	$('#color').click(function(){
+		style = 'color';
+	});
+	$('#default').click(function(){
+		style = 'default';
+	})
 	var cellSize = 560/size;
-	if(size <= 65){
+	if(size <= 128){
 		for(var i = 0; i < size; i++) {
 			$('#sketch').append('<div class="col"></div>')
 		}
@@ -9,25 +16,46 @@ function grid(size) {
 				//code to add a single div which will iterate to input
 				$('.col').append('<div class="cell"></div>');
 		}
+		if(size > 64){
+			$('#gridbut').hide();
+		} else {
+		$('#gridbut').show();
+	}
 	} else {
 		// code to display that input value was too high.
 		console.log(size);
 		}; 
 	$('.cell').css({'height' : cellSize +'px', 'width': cellSize + 'px'});
 	$('.cell').mouseenter(function(){
-		$(this).addClass('blk');
+		if(style === 'default'){
+			$(this).css({'background-color': '#000000'});
+		} else if(style === 'color'){
+			$(this).css({'background-color': randomColor()})
+		}
 	});
 };
 
 
 $(document).ready(function() {
 	grid(16);
+	var gdsize = 16;
 	$('#gridrender').click(function() {
-		var gdsize = $('#gridsize').val();
+		gdsize = $('#gridsize').val();
 		grid(gdsize);
 	});
 	$('#clear').click(function(){
 		$('#sketch').find('div').removeClass('blk');
 	});
+	$('#gridcheck').prop("checked", false);
+	$('#gridcheck').change(function(){
+		if($(this).is(":checked")) {
+			$('.cell').addClass("border");
+		} else {
+			$('.cell').removeClass("border");
+			}
+	});
+
+
+	
  
 });
